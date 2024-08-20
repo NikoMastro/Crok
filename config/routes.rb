ffea042a500de8b86710e85d97f7784b11a9d5c1
Ryuya Inagaki <77526127+gacky86@users.noreply.github.com>
2024-08-20 12:50:17 +0900

    make route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  resources :dogs do
+    resources :health_tracks, only: [:new, :create]
+    resources :medical_records, only: [:new, :create]
+  end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :tasks
+
+  get 'my_family', to: 'families#show', as: :family
+  get 'dogs/:id/health', to: 'dogs#health', as: :dog_health
 end
