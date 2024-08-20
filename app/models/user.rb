c0b7206fea8e464c22d120c5cb7d4bf87e872984
Pratchavee Amornsetthachai <pratchavee.amorn@gmail.com>
2024-08-20 14:00:54 +0900

    save

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  belongs_to :family
+  belongs_to :family, optional: true
   has_many :dogs, through: :family
   has_many :tasks, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_one_attached :photo
 
-  validates :family, presence: true
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :encrypted_password, presence: true
+  # validates :family, presence: true
+  # validates :name, presence: true
+  # validates :email, presence: true, uniqueness: true
+  # validates :encrypted_password, presence: true
 end
