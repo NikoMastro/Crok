a97ed131b07c4e09ec00354728a4d2b187302cc8
Ryuya Inagaki <77526127+gacky86@users.noreply.github.com>
2024-08-20 11:22:29 +0900

    make first seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,54 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# user, dog, family, tasks, comments, health_tracks, medical_records
+
+# family name
+my_family = Family.new(
+  name: "Inagaki"
+)
+my_family.save
+
+# user name role
+ryuya = User.new(
+  name: "Ryuya",
+  role: "Parent"
+)
+
+pierre = User.new(
+  name: "Pierre",
+  role: "Child"
+)
+
+joe = User.new(
+  name: "Joe",
+  role: "Child"
+)
+
+sunjun = User.new(
+  name: "Sunjun",
+  role: "Child"
+)
+ryuya.family = my_family
+pierre.family = my_family
+joe.family = my_family
+sunjun.family = my_family
+ryuya.save
+pierre.save
+joe.save
+sunjun.save
+
+# dog name breed birthday allergies
+doug = Dog.new(
+  name: "Doug",
+  breed: "golden retriever",
+  birthdate: Date.new(2002, 2, 24)
+)
+doug.family = my_family
+doug.save
+
+# tasks
+test_task = Task.new(
+  name: "test",
+  status: false,
+  start_time: "2024-08-19",
+  end_time: "2024-08-19"
+)
+test_task.save
