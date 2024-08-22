5751cdff78e1518c3461d0cb35e717523d4074fa
Ryuya Inagaki <77526127+gacky86@users.noreply.github.com>
2024-08-22 12:18:43 +0900

    repair bug of toggle

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -3,9 +3,16 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="toggle"
 export default class extends Controller {
   static targets = ["togglableDetail", "togglableForm"]
-  show_detail() {
-    this.togglableDetailTarget.classList.toggle("d-none");
+
+  show_detail(event) {
+    // クリックされた要素のIDから対応するターゲットを取得
+    const taskId = event.currentTarget.dataset.taskId;
+    const detailTarget = this.togglableDetailTargets.find(target => target.dataset.taskId === taskId);
+    if (detailTarget) {
+      detailTarget.classList.toggle("d-none");
+    }
   }
+
   show_form() {
     this.togglableFormTarget.classList.toggle("d-none");
   }
