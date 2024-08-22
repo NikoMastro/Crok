1dbd49644ced1cab1047e68e580dcde75f946e8d
Ryuya Inagaki <77526127+gacky86@users.noreply.github.com>
2024-08-22 11:42:05 +0900

    working

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,8 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  static targets = ["togglableElement"]
+  static targets = ["togglableElement", "togglableForm"]
   fire() {
     this.togglableElementTarget.classList.toggle("d-none");
+  }
+  show_form() {
+    this.togglableFormTarget.classList.toggle("d-none");
   }
 }
