54144550bb4f1c491cf432b99b88860006d42b79
pierre nikolas contessotto <pierrenikolas@gmail.com>
2024-08-26 15:52:07 +0900

    add medical & fix invitations

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,7 +1,3 @@
 class InvitationMailer < ApplicationController
 
-  def notification_email
-    @invitation = params[:invitation]
-    mail(to: @invitation.receipient_email, subject: 'You have been invited to join our platform')
-  end
 end
--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,6 +1,3 @@
 class Invitation < ApplicationRecord
-  has_secure_token :has_secure_token
-  belongs_to :user
 
-  validates :receipient_email, presence: true, uniqueness: true
 end
