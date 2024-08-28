972e9ee2c66f2735a334eecb003dfeeec37cc169
pierre nikolas contessotto <pierrenikolas@gmail.com>
2024-08-28 19:15:18 +0900

    html css centralize

--- app/models/medical_record.rb
+++ app/models/medical_record.rb
@@ -1,5 +1,8 @@
 class MedicalRecord < ApplicationRecord
   belongs_to :dog
-  has_many_attached :pictures
+  has_many_attached :photos, dependent: :destroy
 
+  validates :date, presence: true, on: :create
+  validates :description, presence: true, on: :create
+  validates :vet_contact, presence: true, on: :create
 end
