12badc6ba5ab298c6883c45d787f9956515c00d0
pierre nikolas contessotto <pierrenikolas@gmail.com>
2024-09-03 00:21:09 +0900

    mailer done

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -95,4 +95,17 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  #
+
+
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            'your_email@gmail.com',
+    password:             'your_password',
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
